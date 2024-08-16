f45bad21a9bad6cadfd08f2c445d708d94c5406e
Abdulsalam Baruwa <dmystical.coder@gmail.com>
2024-08-16 06:41:16 +0000

    create an express app

--- Introduction-to-ExpressJS/app.js
+++ Introduction-to-ExpressJS/app.js
@@ -2,3 +2,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+
+// create an Express app
+const app = express();
+const port = 3000;
