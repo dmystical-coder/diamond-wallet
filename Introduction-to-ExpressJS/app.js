0aaffa67caa16c555257c1c491d78d8fdd1a8c8b
Abdulsalam Baruwa <dmystical.coder@gmail.com>
2024-08-16 06:43:40 +0000

    define and use middleware

--- Introduction-to-ExpressJS/app.js
+++ Introduction-to-ExpressJS/app.js
@@ -6,3 +6,7 @@ const bodyParser = require('body-parser');
 // create an Express app
 const app = express();
 const port = 3000;
+
+// Middleware
+app.use(morgan('dev')); // Logging middleware
+app.use(bodyParser.json()); // JSON parsing middleware
